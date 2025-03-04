cc21246c036a607a477991af7cc395f9fff2d06c
Corentin-cott <corentin.cott@gmail.com>
2025-03-04 16:53:34 +0100

    Refactorisation du traitement des événements spécifiques aux jeux avec des fonctions et mappages dédiés

--- internal/triggers/actions.go
+++ internal/triggers/actions.go
@@ -31,6 +31,49 @@ func WriteToLogFile(logPath string, line string) error {
 	return nil
 }
 
+// Define the functions for each game, here is Minecraft
+func handleMinecraftPlayerMessage(line string) (string, string, string, string, error) {
+	playerChatRegex := regexp.MustCompile(`\[(\d{2}:\d{2}:\d{2})\] \[Server thread/INFO](?: \[.+?/MinecraftServer])?: <(.+?)> (.+)`)
+	matches := playerChatRegex.FindStringSubmatch(line)
+	if len(matches) < 4 {
+		return "", "", "", "", fmt.Errorf("ERROR WHILE EXTRACTING CHAT PLAYER NAME FOR MINECRAFT")
+	}
+	playerName := matches[2]
+	message := matches[3]
+
+	// Get player UUID and head URL for Minecraft
+	playerUUID, err := services.GetMinecraftPlayerUUID(playerName)
+	if err != nil {
+		return "", "", "", "", fmt.Errorf("ERROR WHILE GETTING PLAYER UUID: %v", err)
+	}
+
+	playerHeadURL, err := services.GetMinecraftPlayerHeadURL(playerUUID)
+	if err != nil {
+		return "", "", "", "", fmt.Errorf("ERROR WHILE GETTING PLAYER HEAD URL: %v", err)
+	}
+
+	titleURL := "https://fr.namemc.com/profile/" + playerUUID
+	return playerName, message, playerHeadURL, titleURL, nil
+}
+
+// Define the functions for each game, here is Palworld
+func handlePalworldPlayerMessage(line string) (string, string, string, string, error) {
+	playerChatRegex := regexp.MustCompile(`\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[CHAT\] <(.+?)> (.+)`)
+	matches := playerChatRegex.FindStringSubmatch(line)
+	if len(matches) < 3 {
+		return "", "", "", "", fmt.Errorf("ERROR WHILE EXTRACTING CHAT PLAYER NAME FOR PALWORLD")
+	}
+	playerName := matches[1]
+	message := matches[2]
+	return playerName, message, "", "", nil
+}
+
+// Create a map for game-specific actions
+var gameActionsMap = map[string]func(string) (string, string, string, string, error){
+	"Minecraft": handleMinecraftPlayerMessage,
+	"Palworld":  handlePalworldPlayerMessage,
+}
+
 // Action when a player message is detected
 func PlayerMessageAction(line string, serverID int) error {
 	// Server infos
@@ -38,69 +81,32 @@ func PlayerMessageAction(line string, serverID int) error {
 	if err != nil {
 		return fmt.Errorf("ERROR WHILE GETTING SERVER BY ID FOR PLAYER MESSAGE: %v", err)
 	}
-	// Player name & message
-	var playerName string
-	var message string
-	if server.Jeu == "Minecraft" {
-		playerChatRegex := regexp.MustCompile(`\[(\d{2}:\d{2}:\d{2})\] \[Server thread/INFO](?: \[.+?/MinecraftServer])?: <(.+?)> (.+)`)
-		matches := playerChatRegex.FindStringSubmatch(line)
-		if len(matches) < 4 {
-			return fmt.Errorf("ERROR WHILE EXTRACTING CHAT PLAYER NAME... MAYBE IT'S NOT A PLAYER MESSAGE ? LINE: %v", line)
-		}
-		playerName = matches[2]
-		message = matches[3]
-	} else if server.Jeu == "Palworld" {
-		playerChatRegex := regexp.MustCompile(`\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[CHAT\] <(.+?)> (.+)`)
-		matches := playerChatRegex.FindStringSubmatch(line)
-		if len(matches) < 3 {
-			return fmt.Errorf("ERROR WHILE EXTRACTING CHAT PLAYER NAME FOR PALWORLD... MAYBE IT'S NOT A PLAYER MESSAGE? LINE: %v", line)
-		}
-		playerName = matches[1]
-		message = matches[2]
-	} else {
-		return fmt.Errorf("ERROR WHILE GETTING PLAYER NAME: SERVER GAME IS NOT SUPPORTED")
-	}
-	// Get player UUID and player head URL for Minecraft servers
-	var playerUUID string
-	var playerHeadURL string
-	var titleURL string
-	if server.Jeu == "Minecraft" {
-		playerUUID, err = services.GetMinecraftPlayerUUID(playerName) // Get player UUID
-		if err != nil {
-			return fmt.Errorf("ERROR WHILE GETTING PLAYER UUID: %v", err)
-		}
 
-		playerHeadURL, err = services.GetMinecraftPlayerHeadURL(playerUUID) // Get player head URL
-		if err != nil {
-			return fmt.Errorf("ERROR WHILE GETTING PLAYER HEAD URL: %v", err)
-		}
-
-		titleURL = "https://fr.namemc.com/profile/" + playerUUID
-	} else {
-		playerUUID = ""
-		playerHeadURL = ""
-		titleURL = ""
+	// Get the appropriate game action function from the map
+	actionFunc, exists := gameActionsMap[server.Jeu]
+	if !exists {
+		return fmt.Errorf("ERROR: SERVER GAME %v IS NOT SUPPORTED", server.Jeu)
 	}
+
+	// Call the specific action function for the game
+	playerName, message, playerHeadURL, titleURL, err := actionFunc(line)
+	if err != nil {
+		return err
+	}
+
 	// Bot config
-	var botName string
-	if server.Jeu == "Minecraft" {
-		botName = "mineotterBot"
-	} else {
-		botName = "multiloutreBot"
-	}
-	// Action
+	botName := "mineotterBot" // Can be extended similarly using the mappage if needed
+
+	// Send the Discord embed message
 	embed := models.EmbedConfig{
 		Title:       playerName,
 		TitleURL:    titleURL,
 		Description: message,
 		Color:       server.EmbedColor,
 		Thumbnail:   playerHeadURL,
-		MainImage:   "",
 		Footer:      "Message venant de " + server.Nom,
-		Author:      "",
-		AuthorIcon:  "",
-		Timestamp:   false,
 	}
+
 	err = discord.SendDiscordEmbedWithModel(config.AppConfig.Bots[botName], config.AppConfig.DiscordChannels.MinecraftChatChannelID, embed)
 	if err != nil {
 		return fmt.Errorf("ERROR WHILE SENDING DISCORD EMBED: %v", err)
@@ -108,32 +114,52 @@ func PlayerMessageAction(line string, serverID int) error {
 	return nil
 }
 
+// Define the functions for each game, here is Minecraft
+func handleMinecraftPlayerJoined(line string) (string, error) {
+	playerJoinedRegex := regexp.MustCompile(`\[(\d{2}:\d{2}:\d{2})\] \[Server thread/INFO](?: \[.+?/MinecraftServer])?: (.+) joined the game`)
+	matches := playerJoinedRegex.FindStringSubmatch(line)
+	if len(matches) < 3 {
+		return "", fmt.Errorf("ERROR WHILE EXTRACTING JOINED PLAYER NAME FOR MINECRAFT SERVER")
+	}
+	return matches[2], nil
+}
+
+// Define the functions for each game, here is Palworld
+func handlePalworldPlayerJoined(line string) (string, error) {
+	playerJoinedRegex := regexp.MustCompile(`\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[LOG\] (.+?) \d{1,3}(?:\.\d{1,3}){3} connected the server`)
+	matches := playerJoinedRegex.FindStringSubmatch(line)
+	if len(matches) < 2 {
+		return "", fmt.Errorf("ERROR WHILE EXTRACTING JOINED PLAYER NAME FOR PALWORLD SERVER")
+	}
+	return matches[1], nil
+}
+
+// Create a map for game-specific actions for joined and left actions
+var gameJoinActionsMap = map[string]func(string) (string, error){
+	"Minecraft": handleMinecraftPlayerJoined,
+	"Palworld":  handlePalworldPlayerJoined,
+}
+
 // Action when a player joined the server
 func PlayerJoinedAction(line string, serverID int) error {
 	// Server infos
 	server, err := db.GetServerById(serverID)
 	if err != nil {
-		return fmt.Errorf("ERROR WHILE GETTING SERVER BY ID FOR MINECRAFT SERVER STOPPED: %v", err)
+		return fmt.Errorf("ERROR WHILE GETTING SERVER BY ID FOR PLAYER JOINED: %v", err)
 	}
-	// Player name
-	var playername string
-	if server.Jeu == "Minecraft" {
-		playerJoinedRegex := regexp.MustCompile(`\[(\d{2}:\d{2}:\d{2})\] \[Server thread/INFO](?: \[.+?/MinecraftServer])?: (.+) joined the game`)
-		matches := playerJoinedRegex.FindStringSubmatch(line)
-		if len(matches) < 3 {
-			return fmt.Errorf("ERROR WHILE EXTRACTING JOINED PLAYER NAME FOR MINECRAFT SERVER")
-		}
-		playername = matches[2]
-	} else if server.Jeu == "Palworld" {
-		playerJoinedRegex := regexp.MustCompile(`\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[LOG\] (.+?) \d{1,3}(?:\.\d{1,3}){3} connected the server`)
-		matches := playerJoinedRegex.FindStringSubmatch(line)
-		if len(matches) < 2 {
-			return fmt.Errorf("ERROR WHILE EXTRACTING JOINED PLAYER NAME FOR PALWORLD SERVER")
-		}
-		playername = matches[1]
-	} else {
-		return fmt.Errorf("ERROR WHILE GETTING PLAYER NAME: SERVER GAME IS NOT SUPPORTED")
+
+	// Get the appropriate game action function from the map for "joined" action
+	actionFunc, exists := gameJoinActionsMap[server.Jeu]
+	if !exists {
+		return fmt.Errorf("ERROR: SERVER GAME %v IS NOT SUPPORTED", server.Jeu)
 	}
+
+	// Call the specific action function for the game
+	playerName, err := actionFunc(line)
+	if err != nil {
+		return err
+	}
+
 	// Bot config
 	var botName string
 	if server.Jeu == "Minecraft" {
@@ -141,22 +167,56 @@ func PlayerJoinedAction(line string, serverID int) error {
 	} else {
 		botName = "multiloutreBot"
 	}
-	// Action
-	discord.SendDiscordEmbed(config.AppConfig.Bots[botName], config.AppConfig.DiscordChannels.MinecraftChatChannelID, playername+" à rejoint "+server.Nom, "", server.EmbedColor)
-	playerID, err := db.CheckAndInsertPlayerWithPlayerName(playername, 1, "now")
+
+	// Send the Discord embed message
+	discord.SendDiscordEmbed(config.AppConfig.Bots[botName], config.AppConfig.DiscordChannels.MinecraftChatChannelID, playerName+" a rejoint "+server.Nom, "", server.EmbedColor)
+
+	// Handle player connection log in DB
+	playerID, err := db.CheckAndInsertPlayerWithPlayerName(playerName, 1, "now")
 	if err != nil {
 		return fmt.Errorf("ERROR WHILE CHECKING OR INSERTING PLAYER: %v", err)
 	}
+
 	err = db.SaveConnectionLog(playerID, serverID)
 	if err != nil {
-		return fmt.Errorf("ERROR WHILE SAVING CONNECTION LOG: FOR PLAYER %v IN DATABASE: %v", playername, err)
+		return fmt.Errorf("ERROR WHILE SAVING CONNECTION LOG: FOR PLAYER %v IN DATABASE: %v", playerName, err)
 	}
+
 	err = db.UpdatePlayerLastConnection(playerID)
 	if err != nil {
-		return fmt.Errorf("ERROR WHILE UPDATING LAST CONNECTION FOR PLAYER %v IN DATABASE: %v", playername, err)
+		return fmt.Errorf("ERROR WHILE UPDATING LAST CONNECTION FOR PLAYER %v IN DATABASE: %v", playerName, err)
 	}
-	WriteToLogFile("/var/log/serversentinel/playerjoined.log", playername)
+
+	// Log to file
+	WriteToLogFile("/var/log/serversentinel/playerjoined.log", playerName)
+
 	return nil
+}
+
+// Define the functions for each game, here is Minecraft
+func handleMinecraftPlayerLeft(line string) (string, error) {
+	playerDisconnectedRegex := regexp.MustCompile(`\[\d{2}:\d{2}:\d{2}\] \[Server thread/INFO\].*?: ([^\s]+) (?:left the game|disconnected|lost connection)`)
+	matches := playerDisconnectedRegex.FindStringSubmatch(line)
+	if len(matches) < 2 || len(matches[1]) < 3 { // Minecraft player names are at least 3 characters long, so this filter prevents false positives
+		return "", fmt.Errorf("ERROR WHILE EXTRACTING DISCONNECTED PLAYER NAME FOR MINECRAFT SERVER")
+	}
+	return matches[1], nil
+}
+
+// Define the functions for each game, here is Palworld
+func handlePalworldPlayerLeft(line string) (string, error) {
+	playerDisconnectedRegex := regexp.MustCompile(`\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[LOG\] (.+?) left the server`)
+	matches := playerDisconnectedRegex.FindStringSubmatch(line)
+	if len(matches) < 2 {
+		return "", fmt.Errorf("ERROR WHILE EXTRACTING LEFT PLAYER NAME FOR PALWORLD SERVER")
+	}
+	return matches[1], nil
+}
+
+// Create a map for game-specific actions for joined and left actions
+var gameLeaveActionsMap = map[string]func(string) (string, error){
+	"Minecraft": handleMinecraftPlayerLeft,
+	"Palworld":  handlePalworldPlayerLeft,
 }
 
 // Action when a player left the server
@@ -164,27 +224,21 @@ func PlayerLeftAction(line string, serverID int) error {
 	// Server infos
 	server, err := db.GetServerById(serverID)
 	if err != nil {
-		return fmt.Errorf("ERROR WHILE GETTING SERVER BY ID FOR MINECRAFT SERVER STOPPED: %v", err)
+		return fmt.Errorf("ERROR WHILE GETTING SERVER BY ID FOR PLAYER LEFT: %v", err)
 	}
-	// Player name
-	var playername string
-	if server.Jeu == "Minecraft" {
-		playerDisconnectedRegex := regexp.MustCompile(`\[\d{2}:\d{2}:\d{2}\] \[Server thread/INFO\].*?: ([^\s]+) (?:left the game|disconnected|lost connection)`)
-		matches := playerDisconnectedRegex.FindStringSubmatch(line)
-		if len(matches) < 2 || len(matches[1]) < 3 { // Minecraft player names are at least 3 characters long, so this filter prevents false positives
-			return fmt.Errorf("ERROR WHILE EXTRACTING DISCONNECTED PLAYER NAME FOR MINECRAFT SERVER")
-		}
-		playername = matches[1]
-	} else if server.Jeu == "Palworld" {
-		playerDisconnectedRegex := regexp.MustCompile(`\[\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2}\] \[LOG\] (.+?) left the server`)
-		matches := playerDisconnectedRegex.FindStringSubmatch(line)
-		if len(matches) < 2 {
-			return fmt.Errorf("ERROR WHILE EXTRACTING JOINED PLAYER NAME FOR PALWORLD SERVER")
-		}
-		playername = matches[1]
-	} else {
-		return fmt.Errorf("ERROR WHILE GETTING PLAYER NAME: SERVER GAME IS NOT SUPPORTED")
+
+	// Get the appropriate game action function from the map for "left" action
+	actionFunc, exists := gameLeaveActionsMap[server.Jeu]
+	if !exists {
+		return fmt.Errorf("ERROR: SERVER GAME %v IS NOT SUPPORTED", server.Jeu)
 	}
+
+	// Call the specific action function for the game
+	playerName, err := actionFunc(line)
+	if err != nil {
+		return err
+	}
+
 	// Bot config
 	var botName string
 	if server.Jeu == "Minecraft" {
@@ -192,8 +246,12 @@ func PlayerLeftAction(line string, serverID int) error {
 	} else {
 		botName = "multiloutreBot"
 	}
-	// Action
-	discord.SendDiscordEmbed(config.AppConfig.Bots[botName], config.AppConfig.DiscordChannels.MinecraftChatChannelID, playername+" à quitté "+server.Nom, "", server.EmbedColor)
-	WriteToLogFile("/var/log/serversentinel/playerdisconnected.log", playername)
+
+	// Send the Discord embed message
+	discord.SendDiscordEmbed(config.AppConfig.Bots[botName], config.AppConfig.DiscordChannels.MinecraftChatChannelID, playerName+" a quitté "+server.Nom, "", server.EmbedColor)
+
+	// Log to file
+	WriteToLogFile("/var/log/serversentinel/playerdisconnected.log", playerName)
+
 	return nil
 }
